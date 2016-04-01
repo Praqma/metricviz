d5b4806ac602c7c3d2ee2b13277e817cee77f6f4
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-04-01 11:23:30 +0200

    Issue #3: Documentation

--- run.bat
+++ run.bat
@@ -1,7 +1,8 @@
 @echo off
 
-:: This script uses the Gradle wrapper. The Java program can also be run with arguments
-:: like this (but then you need to set the classpath):
-:: java -jar build\libs\metricviz-0.1.0.jar --input=doc\input-example.csv --output=output.html
+:: This script uses the Gradle wrapper to run the program.
+:: You can also run standalone like this:
+:: > gradlew.bat installApp
+:: > build\install\metricviz\bin\metricviz.bat --input=doc\input-example.csv --output=output.html
 
 gradlew.bat run -Pinput=doc\input-example.csv -Poutput=output.html
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-## This script uses the Gradle wrapper. The Java program can also be run with arguments
-## like this (but then you need to set the classpath):
-## java -jar build/libs/metricviz-0.1.0.jar --input=doc/input-example.csv --output=output.html
+## This script uses the Gradle wrapper to run the program.
+## You can also run standalone like this:
+## $ ./gradlew installApp
+## $ build/install/metricviz/bin/metricviz --input=doc/input-example.csv --output=output.html
 
 ./gradlew run -Pinput=doc/input-example.csv -Poutput=output.html
